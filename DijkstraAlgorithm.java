039db21e622ff753f7ee98aa1bccce1b69ae3b68
rmcneil30 <rmcneil30@gatech.edu>
2021-05-16 14:27:18 -0400

    first attempt

--- DijkstraAlgorithm.java
+++ DijkstraAlgorithm.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * My implementations of dijkstra's shortest path algorithm. Test cases are my neighborhood.
@@ -12,4 +9,56 @@ import java.util.Map;
 
 public class DijkstraAlgorithm {
 
+    /**
+     * Finds the single-source shortest distance between the start vertex and
+     * all vertices given a weighted graph (assuming non-negative edge
+     * weights).
+     *
+     * Two termination conditions in conjunction.
+     *
+     * 1) Check if all of the vertices have been visited.
+     * 2) Check if the PQ is empty.
+     *
+     *
+     * @param <T>   the generic typing of the data
+     * @param start the vertex to begin the Dijkstra's on (source)
+     * @param graph the graph we are applying Dijkstra's to
+     * @return a map of the shortest distances from start to every
+     * other node in the graph
+     * @throws IllegalArgumentException if any input is null, or if start
+     *                                  doesn't exist in the graph.
+     */
+    public static <T> Map<Vertex<T>, Integer> dijkstras(Vertex<T> start,
+                                                        Graph<T> graph) {
+        if (start == null || graph == null) {
+            throw new IllegalArgumentException("You cannot pass in an illegal start or graph. Can't be traversed!");
+        }
+        if (!graph.getVertices().contains(start)) {
+            throw new IllegalArgumentException("You cannot traverse a graph without a start!");
+        }
+
+        Map<Vertex<T>, Integer> shortestPath = new HashMap<>();
+        HashSet<Vertex<T>> visitedSet = new HashSet<>();
+        PriorityQueue<VertexDistance<T>> pq = new PriorityQueue<>();
+        Map<Vertex<T>, List<VertexDistance<T>>> adjList = graph.getAdjList();
+
+        for (Vertex<T> vertex : graph.getVertices()) {
+            shortestPath.put(vertex, Integer.MAX_VALUE);
+        }
+        pq.add(new VertexDistance<T>(start, 0));
+
+        while (!(pq.isEmpty()) && visitedSet.size() != graph.getVertices().size()) {
+            VertexDistance<T> dist = pq.remove();
+            if (!visitedSet.contains(dist.getVertex())) {
+                visitedSet.add(dist.getVertex());
+                shortestPath.put(dist.getVertex(), dist.getDistance());
+                List<VertexDistance<T>> adj = adjList.get(dist.getVertex());
+                for (VertexDistance<T> vertexDistances : adj) {
+                    pq.add(new VertexDistance<T>(vertexDistances.getVertex(),
+                            vertexDistances.getDistance() + dist.getDistance()));
+                }
+            }
+        }
+        return shortestPath;
+    }
 }
--- ShortestPathTests.java
+++ ShortestPathTests.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,7 +16,6 @@ import static org.junit.Assert.assertEquals;
 
 public class ShortestPathTests {
 
-    private Graph<Integer> directedGraph;
     private Graph<Character> undirectedGraph;
     public static final int TIMEOUT = 200;
 
@@ -29,33 +26,64 @@ public class ShortestPathTests {
 
     /**
      * Creates an undirected graph.
-     * The graph is depicted in the attached.
+     * The graph is depicted in the attached file.
      *
      * @return the completed graph
      */
     private Graph<Character> createUndirectedGraph() {
         Set<Vertex<Character>> vertices = new HashSet<>();
-        for (int i = 65; i <= 70; i++) {
-            vertices.add(new Vertex<Character>((char) i));
+        for (int i = 65; i <= 90; i++) {
+            if (vertices.add(new Vertex<Character>((char) i)) != false) { // not workin for some reason
+                vertices.add(new Vertex<Character>((char) i));
+            }
         }
 
         Set<Edge<Character>> edges = new LinkedHashSet<>();
-        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('B'), 7));
-        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('A'), 7));
-        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('C'), 5));
-        edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('A'), 5));
+        edges.add(new Edge<>(new Vertex<>('H'), new Vertex<>('A'), 2));
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('H'), 2));
+
+        edges.add(new Edge<>(new Vertex<>('H'), new Vertex<>('B'), 15));
+        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('H'), 15));
+
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('R'), 11));
+        edges.add(new Edge<>(new Vertex<>('R'), new Vertex<>('A'), 11));
+
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('R'), 11));
+        edges.add(new Edge<>(new Vertex<>('R'), new Vertex<>('A'), 11));
+
+        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('C'), 1));
+        edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('B'), 1));
+
         edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('D'), 2));
         edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('C'), 2));
-        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('D'), 4));
-        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('A'), 4));
-        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('E'), 1));
-        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('D'), 1));
-        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('E'), 3));
-        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('B'), 3));
-        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('F'), 8));
-        edges.add(new Edge<>(new Vertex<>('F'), new Vertex<>('B'), 8));
-        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('F'), 6));
-        edges.add(new Edge<>(new Vertex<>('F'), new Vertex<>('E'), 6));
+
+        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('E'), 3));
+        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('D'), 3));
+
+        edges.add(new Edge<>(new Vertex<>('R'), new Vertex<>('Q'), 7));
+        edges.add(new Edge<>(new Vertex<>('Q'), new Vertex<>('R'), 7));
+
+        edges.add(new Edge<>(new Vertex<>('R'), new Vertex<>('P'), 9));
+        edges.add(new Edge<>(new Vertex<>('P'), new Vertex<>('R'), 9));
+
+        edges.add(new Edge<>(new Vertex<>('P'), new Vertex<>('Q'), 1));
+        edges.add(new Edge<>(new Vertex<>('Q'), new Vertex<>('P'), 1));
+
+        edges.add(new Edge<>(new Vertex<>('Q'), new Vertex<>('X'), 5));
+        edges.add(new Edge<>(new Vertex<>('X'), new Vertex<>('Q'), 5));
+
+        edges.add(new Edge<>(new Vertex<>('P'), new Vertex<>('X'), 2));
+        edges.add(new Edge<>(new Vertex<>('X'), new Vertex<>('P'), 2));
+
+        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('X'), 6));
+        edges.add(new Edge<>(new Vertex<>('X'), new Vertex<>('D'), 6));
+
+        edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('X'), 10));
+        edges.add(new Edge<>(new Vertex<>('X'), new Vertex<>('C'), 10));
+
+        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('X'), 4));
+        edges.add(new Edge<>(new Vertex<>('X'), new Vertex<>('E'), 4));
+
 
         return new Graph<Character>(vertices, edges);
     }
@@ -65,12 +93,16 @@ public class ShortestPathTests {
         Map<Vertex<Character>, Integer> dijkActual = DijkstraAlgorithm.dijkstras(
                 new Vertex<Character>('D'), undirectedGraph);
         Map<Vertex<Character>, Integer> dijkExpected = new HashMap<>();
-        dijkExpected.put(new Vertex<>('A'), 4);
-        dijkExpected.put(new Vertex<>('B'), 4);
-        dijkExpected.put(new Vertex<>('C'), 2);
-        dijkExpected.put(new Vertex<>('D'), 0);
-        dijkExpected.put(new Vertex<>('E'), 1);
-        dijkExpected.put(new Vertex<>('F'), 7);
+        dijkExpected.put(new Vertex<>('X'), 23);
+        dijkExpected.put(new Vertex<>('A'), 2);
+        dijkExpected.put(new Vertex<>('B'), 15);
+        dijkExpected.put(new Vertex<>('C'), 16);
+        dijkExpected.put(new Vertex<>('D'), 18);
+        dijkExpected.put(new Vertex<>('E'), 21);
+        dijkExpected.put(new Vertex<>('R'), 13);
+        dijkExpected.put(new Vertex<>('Q'), 20);
+        dijkExpected.put(new Vertex<>('B'), 15);
+        dijkExpected.put(new Vertex<>('P'), 21);
 
         assertEquals(dijkExpected, dijkActual);
     }
