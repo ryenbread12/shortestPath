48d1891468d12e6ad5cf130e862db74e5f748c67
rmcneil30 <rmcneil30@gatech.edu>
2021-05-16 13:41:46 -0400

    file frames

--- DijkstraAlgorithm.java
+++ DijkstraAlgorithm.java
@@ -4,17 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Your implementations of various string searching algorithms.
+ * My implementations of dijkstra's shortest path algorithm. Test cases are my neighborhood.
  *
  * @author Ryan McNeil
  * @version 1.0
- * @userid rmcneil30
- * @GTID 903603007
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
  */
+
 public class DijkstraAlgorithm {
 
 }
---
+++ ShortestPathTests.java
@@ -0,0 +1,78 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class to examine algorithm.
+ */
+
+public class ShortestPathTests {
+
+    private Graph<Integer> directedGraph;
+    private Graph<Character> undirectedGraph;
+    public static final int TIMEOUT = 200;
+
+    @Before
+    public void init() {
+        undirectedGraph = createUndirectedGraph();
+    }
+
+    /**
+     * Creates an undirected graph.
+     * The graph is depicted in the attached.
+     *
+     * @return the completed graph
+     */
+    private Graph<Character> createUndirectedGraph() {
+        Set<Vertex<Character>> vertices = new HashSet<>();
+        for (int i = 65; i <= 70; i++) {
+            vertices.add(new Vertex<Character>((char) i));
+        }
+
+        Set<Edge<Character>> edges = new LinkedHashSet<>();
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('B'), 7));
+        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('A'), 7));
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('C'), 5));
+        edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('A'), 5));
+        edges.add(new Edge<>(new Vertex<>('C'), new Vertex<>('D'), 2));
+        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('C'), 2));
+        edges.add(new Edge<>(new Vertex<>('A'), new Vertex<>('D'), 4));
+        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('A'), 4));
+        edges.add(new Edge<>(new Vertex<>('D'), new Vertex<>('E'), 1));
+        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('D'), 1));
+        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('E'), 3));
+        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('B'), 3));
+        edges.add(new Edge<>(new Vertex<>('B'), new Vertex<>('F'), 8));
+        edges.add(new Edge<>(new Vertex<>('F'), new Vertex<>('B'), 8));
+        edges.add(new Edge<>(new Vertex<>('E'), new Vertex<>('F'), 6));
+        edges.add(new Edge<>(new Vertex<>('F'), new Vertex<>('E'), 6));
+
+        return new Graph<Character>(vertices, edges);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testDijkstras() {
+        Map<Vertex<Character>, Integer> dijkActual = DijkstraAlgorithm.dijkstras(
+                new Vertex<Character>('D'), undirectedGraph);
+        Map<Vertex<Character>, Integer> dijkExpected = new HashMap<>();
+        dijkExpected.put(new Vertex<>('A'), 4);
+        dijkExpected.put(new Vertex<>('B'), 4);
+        dijkExpected.put(new Vertex<>('C'), 2);
+        dijkExpected.put(new Vertex<>('D'), 0);
+        dijkExpected.put(new Vertex<>('E'), 1);
+        dijkExpected.put(new Vertex<>('F'), 7);
+
+        assertEquals(dijkExpected, dijkActual);
+    }
+
+}
